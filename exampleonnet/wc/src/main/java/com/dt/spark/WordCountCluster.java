210b731a84a6ce64f5c428edc1824435ac8946a1
holdbelief <81971357@qq.com>
2018-02-11 22:19:48 +0800

    1

--- exampleonnet/wc/src/main/java/com/dt/spark/WordCountCluster.java
+++ exampleonnet/wc/src/main/java/com/dt/spark/WordCountCluster.java
@@ -1,9 +1,9 @@
 package com.dt.spark;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.apache.spark.SparkConf;
-import org.apache.spark.SparkContext;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
